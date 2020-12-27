abc6e1d683804238f37bdca68d5e88bf3243761e
Kirill Belov <KirillBelovTest@gmail.com>
2020-12-27 15:37:41 +0400

    added first version of tests

--- test_order_book.py
+++ test_order_book.py
@@ -1,5 +1,105 @@
 from order_book import Order
 from order_book import OrderBook
 
-def test_00_debug():
-    assert True
+def test_succes_order_book_01_create():
+    book = OrderBook()
+
+    assert type(book.orders) == dict
+    assert len(book.orders) == 0
+
+def test_succes_add_order_01_one_order():
+    book = OrderBook()
+    order = Order("sell", 6, 1)
+    id = book.add_order(order)
+
+    assert id == order.id
+    assert order.state == "opened"
+    assert len(book.orders) == 1
+    assert list(book.orders.values()) == [order]
+    assert list(book.orders.keys()) == [order.id]
+
+def test_success_add_order_02_several_orders():
+    book = OrderBook()
+    order1 = Order("buy", 3, 4)
+    order2 = Order("sell", 7, 5)
+    book.add_order(order1)
+    book.add_order(order2)
+
+    assert len(book.orders) == 2
+    assert list(book.orders.values()) == [order1, order2]
+    assert list(book.orders.keys()) == [order1.id, order2.id]
+
+def test_success_del_order_01_one_order():
+    book = OrderBook()
+    order = Order("buy", 2, 6)
+    book.add_order(order)
+    order2 = book.del_order(order.id)
+
+    assert order2 == order
+    assert len(book.orders) == 0
+    assert order.state == "canceled"
+
+def test_succes_get_order_01_one_order():
+    book = OrderBook()
+    order = Order("sell", 8, 7)
+    book.add_order(order)
+    order2 = book.get_order(order.id)
+
+    assert order.state == "opened"
+    assert order2 == order
+    assert len(book.orders) == 1
+    assert [book.orders] == { order2.id: order2 }
+
+def test_market_data_01_check_struct():
+    book = OrderBook()
+    order1 = Order("buy", 1, 8)
+    order2 = Order("sell", 9, 9)
+    book.add_order(order1)
+    book.add_order(order2)
+    market = book.market_data()
+
+    assert market == { 
+        "asks": [{ "price": order2.price, "quantity": order2.quantity }], 
+        "bids": [{ "price": order1.price, "quantity": order1.quantity }] 
+    }
+
+def test_market_data_02_check_sorting():
+    book = OrderBook()
+    order1 = Order("buy", 1, 1)
+    order2 = Order("sell", 9, 2)
+    order3 = Order("buy", 3, 3)
+    order4 = Order("sell", 7, 4)
+    order5 = Order("buy", 2, 5)
+    order6 = Order("sell", 8, 6)
+    
+    book.add_order(order1)
+    book.add_order(order2)
+    book.add_order(order3)
+    book.add_order(order4)
+    book.add_order(order5)
+    book.add_order(order6)
+    
+    market = book.market_data()
+
+    assert market["asks"] == sorted(market["asks"], key = lambda d: d["price"], reverse = True)
+    assert market["bids"] == sorted(market["bids"], key = lambda d: d["price"], reverse = True)
+
+def test_market_data_03_check_agregation():
+    book = OrderBook()
+    order1 = Order("buy", 1, 1)
+    order2 = Order("sell", 2, 1)
+    order3 = Order("buy", 1, 1)
+    order4 = Order("sell", 2, 1)
+    
+    book.add_order(order1)
+    book.add_order(order2)
+    book.add_order(order3)
+    book.add_order(order4)
+
+    market = book.market_data()
+
+    assert market == { 
+        "asks": [{ "price": 2.0, "quantity": 2.0 }], 
+        "bids": [{ "price": 1.0, "quantity": 2.0 }] 
+    }
+
