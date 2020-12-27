8598fc5011f3be8bae73d7d4bfd9fae5459911f0
Kirill Belov <KirillBelovTest@gmail.com>
2020-12-27 16:06:00 +0400

    fixed all bugs that found by tests in v0.1
    - implemented changing of the order state when order add/del
    - implemented agregation for marked data
    - small fixed - added return types to signatures

--- order_book.py
+++ order_book.py
@@ -22,21 +22,24 @@ class OrderBook:
         Returns unique generated order id.
         If order with the same id exists in the order book then raise exception. 
         """
+        order.state = "opened"
         self.orders[order.id] = order
         return order.id
 
-    def get_order(self, id: int):
+    def get_order(self, id: int) -> Order:
         """Get order from internal dict by id.
         Returns order with passed id or raise exception if this order doesn't exists.
         """
         return self.orders[id]
 
-    def del_order(self, id: int): 
+    def del_order(self, id: int) -> Order: 
         """Delete ordere from order book.
         Returns order with passed id or raise exception if this order doesn't exists. 
         Change order state to 'canceled'.
         """
-        return self.orders.pop(id)
+        order = self.orders.pop(id)
+        order.state = "canceled"
+        return order
 
     def market_data(self) -> dict:
         """Returns representation of all orders as a dictionary with keys 'asks' and 'bids'. 
@@ -46,9 +49,21 @@ class OrderBook:
 
         asks = filter(lambda o: o.side == "sell", orders)
         asks = list(map(lambda o: { "price": o.price, "quantity": o.quantity }, asks))
-        
+        if len(asks) > 1:
+            for i in range(len(asks) - 1):
+                if asks[i]["price"] == asks[i + 1]["price"]:
+                    asks[i + 1]["quantity"] = asks[i + 1]["quantity"] + asks[i]["quantity"]
+                    asks[i]["quantity"] = 0
+        asks = list(filter(lambda d: d["quantity"] > 0, asks))
+
         bids = filter(lambda o: o.side == "buy", orders)
         bids = list(map(lambda o: { "price": o.price, "quantity": o.quantity }, bids))
+        if len(bids) > 1:
+            for i in range(len(bids) - 1):
+                if bids[i]["price"] == bids[i + 1]["price"]:
+                    bids[i + 1]["quantity"] = bids[i + 1]["quantity"] + bids[i]["quantity"]
+                    bids[i]["quantity"] = 0
+        bids = list(filter(lambda d: d["quantity"] > 0, bids))
 
         return { "asks": asks, "bids": bids }
 
